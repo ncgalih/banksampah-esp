72677bc9c1c60842277a2471f3e3019f71070272
Nikolas Galih Saputro <encegalih@gmail.com>
2024-10-31 22:40:29 +0700

    Update banksampah31.ino

--- banksampah31.ino
+++ banksampah31.ino
@@ -4,23 +4,34 @@
 #include <WiFi.h>
 #include <MFRC522.h>
 #include <SPI.h>
+#include <Wire.h>
 
 // Pin definisi
 #define SS_PIN 21
 #define RST_PIN 22
 
+#define SDA_PIN 15  // Pin SDA baru
+#define SCL_PIN 14  // Pin SCL baru
+#define I2C_ADDRESS 8  // Alamat I2C untuk slave ESP32
+
+#define TRIG_PIN_A 4
+#define ECHO_PIN_A 5
+
 MFRC522 rfid(SS_PIN, RST_PIN);
 
 EasyNex myNex(Serial2);  // Gunakan Serial2 jika menggunakan ESP32
 
-const char* ssid = "Redmi 8";           // Replace with your Wi-Fi SSID
-const char* password = "ngampilan";
+const char* ssid = "Niken";           // Replace with your Wi-Fi SSID
+const char* password = "12345678";
 
 String currentUser = "";
 String currentAlamat = "";
 int currentSaldo = 0;
 String cardID = "";
 boolean accessGranted = false;
+String currentType = "";
+String currentSubType = "";
+int countItem = 0;
 
 boolean loginRFID(String rfid){
     HTTPClient http;
@@ -135,6 +146,10 @@ void setup() {
   Serial.begin(115200);
   Serial2.begin(9600);     // Pastikan baud rate sesuai dengan pengaturan di Nextion Editor
   myNex.begin();
+
+  changePage(0);
+
+  Wire.begin(SDA_PIN, SCL_PIN);
   
   // Atur teks pada komponen Nextion
   myNex.writeStr("t0", "tes nama nasabah");
@@ -154,8 +169,6 @@ void setup() {
   Serial.println("\nConnected to Wi-Fi");
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
-
-  changePage(0);
 }
 
 void loop() {
@@ -174,7 +187,7 @@ void loop() {
         myNex.writeStr("t1.txt", currentUser);
         myNex.writeStr("t2.txt", currentAlamat);
         delay(1000);
-        changePage(1);   
+        changePage(1);           
       }
       else {
         Serial.println("kartu tidak terdaftar");
@@ -206,9 +219,218 @@ void loop() {
       case 5:
         if (changedPage) mutasiRekening();
         break;
+      case 7:
+        currentType = "plastik";
+        if (myNex.readNumber("setor_plastik.b0.val") == 1) {  // Botol Bening
+            Serial.println("Gambar Botol Bening Ditekan");
+            currentSubType = "PET Botol Bening";
+            Wire.beginTransmission(I2C_ADDRESS);
+            Serial.println("Buka servo plastik");
+            Wire.write(1);
+            Wire.endTransmission();  
+            myNex.writeNum("setor_plastik.b0.val", 0);
+            changePage(11);
+        }
+
+        if (myNex.readNumber("setor_plastik.b1.val") == 1) {  // Botol Warna
+            Serial.println("Gambar Botol Warna Ditekan");
+            currentSubType = "PET Botol Warna";
+            Wire.beginTransmission(I2C_ADDRESS);
+            Wire.write(1);
+            Wire.endTransmission();  
+            myNex.writeNum("setor_plastik.b1.val", 0);
+            changePage(11);
+        }
+
+        if (myNex.readNumber("setor_plastik.b2.val") == 1) {  // Plastik Kemasan
+            Serial.println("Gambar Plastik Kemasan Ditekan");
+            currentSubType = "Plastik Kemasan";
+            Wire.beginTransmission(I2C_ADDRESS);
+            Wire.write(1);
+            Wire.endTransmission();  
+            myNex.writeNum("setor_plastik.b2.val", 0);
+            changePage(11);
+        }
+
+        if (myNex.readNumber("setor_plastik.b3.val") == 1) {  // Tutup Botol
+            Serial.println("Gambar Tutup Botol Ditekan");
+            currentSubType = "Tutup Botol";
+            Wire.beginTransmission(I2C_ADDRESS);
+            Wire.write(1);
+            Wire.endTransmission();  
+            myNex.writeNum("setor_plastik.b3.val", 0);
+            changePage(11);
+        }
+        break;
+      case 8:
+        currentType = "kertas";
+        if (myNex.readNumber("setor_kertas.b0.val") == 1) {  // Arsip
+            Serial.println("Gambar Arsip Ditekan");
+            currentSubType = "Arsip (hvs, buku)";
+            Wire.beginTransmission(I2C_ADDRESS);
+            Wire.write(2);
+            Wire.endTransmission();
+            myNex.writeNum("setor_kertas.b0.val", 0);
+            changePage(11);
+        }
+
+        if (myNex.readNumber("setor_kertas.b1.val") == 1) {  // Tetra Pack
+            Serial.println("Gambar Tetra Pack Ditekan");
+            currentSubType = "Tetra Pack";
+            Wire.beginTransmission(I2C_ADDRESS);
+            Wire.write(2);
+            Wire.endTransmission();
+            myNex.writeNum("setor_kertas.b1.val", 0);
+            changePage(11);
+        }
+
+        if (myNex.readNumber("setor_kertas.b2.val") == 1) {  // Kardus
+            Serial.println("Gambar Kardus Ditekan");
+            currentSubType = "Kardus";
+            Wire.beginTransmission(I2C_ADDRESS);
+            Wire.write(2);
+            Wire.endTransmission();
+            myNex.writeNum("setor_kertas.b2.val", 0);
+            changePage(11);
+        }
+
+        if (myNex.readNumber("setor_kertas.b3.val") == 1) {  // Majalah
+            Serial.println("Gambar Majalah Ditekan");
+            currentSubType = "Majalah/Koran";
+            Wire.beginTransmission(I2C_ADDRESS);
+            Wire.write(2);
+            Wire.endTransmission();
+            myNex.writeNum("setor_kertas.b3.val", 0);
+            changePage(11);
+        }
+        break;
+      case 9:
+        currentType = "logam";
+        if (myNex.readNumber("setor_logam.b0.val") == 1) {  // Seng
+            Serial.println("Gambar Seng Ditekan");
+            currentSubType = "Seng (kaleng)";
+            Wire.beginTransmission(I2C_ADDRESS);
+            Wire.write(3);
+            Wire.endTransmission();
+            myNex.writeNum("setor_logam.b0.val", 0);
+            changePage(11);
+        }
+
+        if (myNex.readNumber("setor_logam.b1.val") == 1) {  // Besi
+            Serial.println("Gambar Besi Ditekan");
+            currentSubType = "Besi";
+            Wire.beginTransmission(I2C_ADDRESS);
+            Wire.write(3);
+            Wire.endTransmission();
+            myNex.writeNum("setor_logam.b1.val", 0);
+            changePage(11);
+        }
+
+        if (myNex.readNumber("setor_logam.b2.val") == 1) {  // Aluminium
+            Serial.println("Gambar Aluminium Ditekan");
+            currentSubType = "Aluminium";
+            Wire.beginTransmission(I2C_ADDRESS);
+            Wire.write(3);
+            Wire.endTransmission();
+            myNex.writeNum("setor_logam.b2.val", 0);
+            changePage(11);
+        }
+
+        if (myNex.readNumber("setor_logam.b3.val") == 1) {  // Tembaga
+            Serial.println("Gambar Tembaga Ditekan");
+            currentSubType = "Tembaga";
+            Wire.beginTransmission(I2C_ADDRESS);
+            Wire.write(3);
+            Wire.endTransmission();
+            myNex.writeNum("setor_logam.b3.val", 0);
+            changePage(11);
+        }
+        break;
+      case 10:
+        currentType = "kaca";
+        if (myNex.readNumber("setor_kaca.b0.val") == 1) {  // Beling
+            Serial.println("Gambar Beling Ditekan");
+            String buffer = myNex.readStr("setor_kaca.t2.txt");
+            currentSubType = buffer;
+            Wire.beginTransmission(I2C_ADDRESS);
+            Wire.write(4);
+            Wire.endTransmission();
+            myNex.writeNum("setor_kaca.b0.val", 0);
+        }
+
+        if (myNex.readNumber("setor_kaca.b1.val") == 1) {  // Botol Kecap
+            Serial.println("Gambar Botol Kecap Ditekan");
+            String buffer = myNex.readStr("setor_kaca.t4.txt");
+            currentSubType = buffer;
+            Wire.beginTransmission(I2C_ADDRESS);
+            Wire.write(4);
+            Wire.endTransmission();
+            myNex.writeNum("setor_kaca.b1.val", 0);
+        }
+
+        if (myNex.readNumber("setor_kaca.b2.val") == 1) {  // Botol Utuh
+            Serial.println("Gambar Botol Utuh Ditekan");
+            String buffer = myNex.readStr("setor_kaca.t6.txt");
+            currentSubType = buffer;
+            Wire.beginTransmission(I2C_ADDRESS);
+            Wire.write(4);
+            Wire.endTransmission();
+            myNex.writeNum("setor_kaca.b2.val", 0);
+        }
+
+        if (myNex.readNumber("setor_kaca.b3.val") == 1) {  // Botol Hijau
+            Serial.println("Gambar Botol Hijau Ditekan");
+            String buffer = myNex.readStr("setor_kaca.t8.txt");
+            currentSubType = buffer;
+            Wire.beginTransmission(I2C_ADDRESS);
+            Wire.write(4);
+            Wire.endTransmission();
+            myNex.writeNum("setor_kaca.b3.val", 0);
+        }
+        break;
+      case 11:
+        if (myNex.readNumber("konfirm_setor.b4.val") == 1){
+          if(currentType == "plastik"){
+            //processStorageA(currentSubType);
+            Wire.beginTransmission(I2C_ADDRESS);
+            Wire.write(5);
+            Wire.endTransmission();
+          }
+          else if(currentType == "kertas"){
+            //processStorageB(currentSubType);
+            Wire.beginTransmission(I2C_ADDRESS);
+            Wire.write(6);
+            Wire.endTransmission();
+          }
+          else if(currentType == "logam"){
+            //processStorageC(currentSubType);
+            Wire.beginTransmission(I2C_ADDRESS);
+            Wire.write(7);
+            Wire.endTransmission();
+          }
+          else if(currentType == "kaca"){
+            //processStorageD(currentSubType);
+            Wire.beginTransmission(I2C_ADDRESS);
+            Wire.write(8);
+            Wire.endTransmission();
+          }
+          changePage(12);
+          delay(250);
+          myNex.writeNum("konfirm_setor.b4.val", 0);
+        }
     }
     currentPage = newPage;
   }
+  //if (detectItems(TRIG_PIN_A, ECHO_PIN_A)){
+  //  countItem ++;
+  //  Serial.print("ada item " + countItem);
+  //}
+
+  // cek koneksi
+  //Wire.beginTransmission(I2C_ADDRESS);
+  //Wire.write(1);
+  //Wire.endTransmission();  
+  //delay(1000);
 }
 
 // belum diedit
@@ -259,4 +481,17 @@ void mutasiRekening() {
   } else {
     myNex.writeStr("r1c1", "WiFi Error");
   }
+}
+
+int detectItems(int trigPin, int echoPin) {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  long duration = pulseIn(echoPin, HIGH, 20000);
+  int distance = duration * 0.034 / 2;  // Distance in cm
+
+  return (distance < 10) ? 1 : 0;
 }
